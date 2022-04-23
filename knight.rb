4a53ff31a30f454ae51659f50653d15cb4fbd50e
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-04-23 14:18:41 +0200

    Add a support method to find neighbors on the graph

---
+++ knight.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+class Knight
+
+  def find_neighbors(nodes, node, digit1, digit2)
+
+    # readability
+    coords = node.position
+
+    # check if a neighbor can exist on the board
+    if (coords[0]+digit1 >= 0) &&
+    (coords[0]+digit1 <= 7) &&
+    (coords[1]+digit2 >= 0) &&
+    (coords[1]+digit2 <= 7)
+      # if yes, find it in the nodes list
+      nodes.each do |neighbor|
+        if neighbor.position == [coords[0]+digit1,coords[1]+digit2]
+          # add it as a neighbor
+          node.neighbors.push(neighbor)
+        end
+      end
+    end
+  end
+end
+
